1d1de2145101f0d5b7f7567eb4d902049a0a3d45
Arnab Majumder <arnabmajumdar399@gmail.com>
2024-03-31 02:39:33 +0530

    register user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,7 +4,7 @@ import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
-const registerUser = asyncHandler( async(req , res) => {
+const registerUser = asyncHandler (async(req , res) => {
     
     // steps for registering a user 
     // 1. get user details from frontend
@@ -19,26 +19,34 @@ const registerUser = asyncHandler( async(req , res) => {
     // 9. check for the return response
 
     const {fullName , email , password , username} = req.body
-    console.log("Email is : ", email)
+    // console.log("Email is : ", email)
 
     // 1. get user details from frontend
-    if ([fullName , email , password , username].some((field) => field?.trim() === "" )) {
+    if (
+        [fullName , email , password , username].some((field) => field?.trim() === "" )
+    ) {
         throw new ApiError(400 , "All fields are required")
     }
 
     
     // 3. check if user already exist or not through 'username' or 'email'
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or : [{email}, {password}]
     })
     if (existedUser) {
         throw new ApiError(409 , "User with email or username already exists")
     }
 
-
     // 4. check for images, check for avatar
     const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path
+    let coverImageLocalPath
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+    
+    
     if (!avatarLocalPath) {
         throw new ApiError(400 , "Avatar is required")
     }
@@ -50,7 +58,7 @@ const registerUser = asyncHandler( async(req , res) => {
 
 
     // 5.1 check if the avatar is successfully uploaded or not
-    if(avatar){
+    if(!avatar){
         throw new ApiError(400 , "Avatar is required")
     }
 
